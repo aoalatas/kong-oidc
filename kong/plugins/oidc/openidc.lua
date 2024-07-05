18b32663f2e083e66593decddc6f8e7ad5b4104a
Ali Osman Alataş <aliosman.alatas@dgpaysit.com>
2024-07-05 11:58:28 +0300

    add code

--- kong/plugins/oidc/openidc.lua
+++ kong/plugins/oidc/openidc.lua
@@ -372,7 +372,7 @@ local function openidc_authorize(opts, session, target_url, prompt)
   
   log(WARN, "ngx redirect encoded uri: " .. encodedUrlPart)
 
-  --params.redirect_uri = params.redirect_uri .. "%2F%23%2Fx%2Fy%2Fz"
+  params.redirect_uri = params.redirect_uri .. encodedUrlPart
 
   if nonce then
     params.nonce = nonce
